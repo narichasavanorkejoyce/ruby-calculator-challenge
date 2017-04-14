5dcc7923c1843937a74cdd4d51a47baf582b511c
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-14 13:41:30 -0400

    Challenge Attempt #1

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,39 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+  def initialize(starting_number)
+    @value = starting_number
+  end
+
+  def addition(num)
+    @sum = @value + num
+    @value = @sum
+    self
+  end
+
+  def subtraction(num)
+    @subtract = @value - num
+    @value = @subtract
+    self
+  end
+
+  def multiplication(num)
+    @product = @value * num
+    @value = @product
+    self
+  end
+
+  def division(num)
+    @divide = @value / num
+    @value = @divide
+    self
+  end
+
+  def equals
+    @value
+  end
+
+  def clear
+    @value = 0
+  end
+end
